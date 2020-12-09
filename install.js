602fc396a593cd36fd2aa33ba293b1a6f1135300
raymond wang <waymondrang@gmail.com>
2020-12-08 23:48:55 -0800

    delete index.old.js, update config template

--- install.js
+++ install.js
@@ -1,15 +1,12 @@
 const colors = require('./colors');
 var Service = require('node-windows').Service;
 
-// create new service object
 var svc = new Service({
-  name: 'StudentVue Mailer',
+  name: 'StudentVue Mailer Beta',
   description: 'The StudentVue Mailer will check for new Synergy emails at a set interval, and them forward them to other email addresses.',
   script: `${__dirname}\\index.js`
 });
 
-// listen for the "install" event, which indicates the
-// process is available as a service.
 svc.on('install', function () {
   svc.start();
   console.log(`[${new Date().toLocaleString()}] ${colors.green}StudentVue Mailer is now running as a Windows Service. It will run in the background until it is manually terminated.${colors.r}`)
